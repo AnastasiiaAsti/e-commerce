3f670281125467cd9caff4fd24473855eb00b2ef
Asti <astistateofmind@gmail.com>
2022-10-19 15:54:22 -0700

    create props for items and fill products with prop

--- src/Product.js
+++ src/Product.js
@@ -2,18 +2,24 @@ import React from 'react';
 import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
 
-function Product() {
+function Product({ id, title, image, price, rating }) {
   return (
           <div className="product">
               <div className="product__info">
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, nam!</p>
+              <p>{title}</p>
                   <p className="prodect__price">
                       <small>$</small>
-                      <strong>30</strong>
+                  <strong>{price}</strong>
                   </p>
-                  <div className="product__rating"><StarIcon /><StarIcon /><StarIcon /></div>
+              <div className="product__rating">
+                  {Array(rating)
+                      .fill()
+                      .map((_, i) => (
+                        <StarIcon />
+                  ))}
+                  </div>
               </div>
-              <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
+              <img src={image} alt="" />
 
               <button>Add To Basket</button>
           </div>
