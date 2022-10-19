15799b099880dfb54196b493440fe29f90f90370
Asti <astistateofmind@gmail.com>
2022-10-19 12:36:58 -0700

    styling to home page and product component

--- src/Product.js
+++ src/Product.js
@@ -8,7 +8,10 @@ function Product() {
           <div className="product">
               <div className="product__info">
                   <p>Title</p>
-                  <p className="prodect__price">$30</p>
+                  <p className="prodect__price">
+                      <small>$</small>
+                      <strong>30</strong>
+                  </p>
                   <div className="product__rating"><StarIcon /><StarIcon /><StarIcon /></div>
               </div>
               <img src="https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="" />
