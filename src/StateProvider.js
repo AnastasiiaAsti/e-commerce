679583f135d1a79d788165f21b1642f46cb735d1
Asti <astistateofmind@gmail.com>
2022-10-20 14:48:58 -0700

    add reducer, state provider

---
+++ src/StateProvider.js
@@ -0,0 +1,18 @@
+import React, { Children, createContext, useContext, useReducer } from 'react';
+
+//preps the data layer
+export const StateContext = createContext();
+
+
+//wrap our app and provide the data layer
+export const StateProvider = ({ reducer, initialState, children }) => {
+    <StateContext.Provider
+        value={useReducer(reducer, initialState)}>
+        {children}
+    </StateContext.Provider>
+
+};
+
+//pull info from the data layer
+export const useStateValue = () => useContext(StateContext);
+
---
+++ src/reducer.js
@@ -0,0 +1,13 @@
+// here define all the application level states and define actions to make changes to the state
+
+export const initialState = {
+    basket: [],
+}
+
+// Selector
+
+const reducer = (state, action) => {
+
+}
+
+export default reducer;
