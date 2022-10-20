4d8487652abc2520b6014e41f6d38334a7e5553a
Asti <astistateofmind@gmail.com>
2022-10-20 11:47:33 -0700

    create checkoutProduct component

--- src/components/CheckoutProduct/CheckoutProduct.js
+++ src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import "./CheckoutProduct.css";
+import StarIcon from '@mui/icons-material/Star';
 
 export default function CheckoutProduct() {
   return (
-    <div>CheckoutProduct</div>
+    <div className='checkoutProduct'>
+      <img src="https://images.unsplash.com/photo-1560343090-f0409e92791a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60" alt="" className="checkoutProduct__image" />
+
+      <div className="product__info">
+        <p className="checkoutProduct__title">Suede green boots that would break hearts and wallets</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          <strong>500</strong>
+        </p>
+        <div className="checkoutProduct__rating"><StarIcon/><StarIcon/><StarIcon/></div>
+      </div>
+    </div>
   )
 }
