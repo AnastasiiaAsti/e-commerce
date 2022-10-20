aef213721cf7d4ed365faa683fa24b3edb01612b
Asti <astistateofmind@gmail.com>
2022-10-20 16:30:14 -0700

    move data into data.js

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const data = {
+    products: [
+        {
+            id: "00001",
+            title: "Fancy black headphones on yellow background",
+            price: 30,
+            rating: 5,
+            image: "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+        },
+        {
+            id: "00002",
+            title: "White minimal watch on white background",
+            price: 100,
+            rating: 4,
+            image: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+        },
+        {
+            id:"00003",
+            title: "Suede green boots that would break hearts and wallets",
+            price: 500,
+            rating: 5,
+            image: "https://images.unsplash.com/photo-1560343090-f0409e92791a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+        },
+        {
+            id:"00004",
+            title: "Classic ray-ban shades",
+            price: 60,
+            rating: 2,
+            image: "https://images.unsplash.com/photo-1572635196237-14b3f281503f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8cHJvZHVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1600&q=60"
+        },
+        {
+            id: "00005",
+            title: "A mini version of a mini car",
+            price: 88,
+            rating: 5,
+            image: "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=1600&q=60"
+        },
+        {
+            id: "00006",
+            title: "Colorfull heels",
+            price: 150,
+            rating: 3,
+            image: "https://images.unsplash.com/photo-1543163521-1bf539c55dd2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHByb2R1Y3R8ZW58MHx8MHx8&auto=format&fit=crop&w=1600&q=60"
+                    
+        }
+    ]
+}
