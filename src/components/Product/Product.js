7a9dcbf9bae8f13d54d92439cabed309118a0417
Asti <astistateofmind@gmail.com>
2022-10-21 14:37:30 -0700

    add items to cart feature

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import "./Product.css";
 import StarIcon from '@mui/icons-material/Star';
+import { useStateValue } from "../../StateProvider";
 
 function Product(props) {
+    const [state, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: props.id,
+                image: props.image,
+                price: props.price,
+                rating: props.rating,
+            },
+        });
+    };
+
   return (
           <div className="product">
               <div className="product__info">
@@ -21,7 +36,7 @@ function Product(props) {
               </div>
               <img src={props.product.image} alt="" />
 
-              <button>Add To Basket</button>
+              <button onClick={addToBasket}>Add To Basket</button>
           </div>
   )
 }
